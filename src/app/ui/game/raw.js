1fb7189d02fb2f4d889124865d3d568f52abd668
Louis Labeyrie <labeyrielouis@gmail.com>
2023-12-08 07:00:58 +0100

    WIP

---
+++ src/app/ui/game/raw.js
@@ -0,0 +1,208 @@
+export default function page() {
+    return (
+       <div className="global">
+          <p>
+             <p className="underline">Idées Reçues Fausses</p> <br></br>
+             <p className="font-semibold">
+                Le réchauffement climatique est trop avancé pour être inversé ou
+                ralenti.
+             </p>{' '}
+             <br></br>
+             FAUX, « Si nous opérons les bons choix en matière de politique,
+             d’infrastructures et de technologies, nous pourrons changer nos
+             modes de vie et nos comportements, avec à la clé une diminution de
+             40 à 70 % des émissions de gaz à effet de serre d’ici à 2050 ».
+             Priyadarshi Shukla, coprésident du groupe de travail III du GIEC.{' '}
+             <br></br>
+             <p className="font-semibold">
+                Les petits gestes individuels ne font aucune différence dans la
+                lutte contre le changement climatique.
+             </p>
+             <br></br>
+             FAUX, afin de simples petits gestes nous pouvons faire la différence
+             . par exemple,réduire la consommation de viande rouge d'une seule
+             personne peut économiser environ 912,5 kg de CO2 par an, utiliser
+             des vélos ou marcher au lieu de conduire pour des trajets courts
+             peut économiser environ 547,5 kg de CO2 par personne et par an,
+             remplacer une ampoule traditionnelle par une LED peut réduire les
+             émissions de CO2 d'environ 29 kg par an. <br></br>
+             <p className="font-semibold">
+                Le changement climatique n'affecte que les régions polaires et
+                les petits îlots.{' '}
+             </p>
+             <br></br>
+             Faux, Le changement climatique affecte le monde entier, pas
+             seulement les régions polaires et les petits îlots. Il provoque des
+             phénomènes météorologiques extrêmes, perturbe l'agriculture mondiale
+             et cause l'élévation du niveau de la mer, menaçant les zones
+             côtières partout sur la planète. <br></br>
+             <p className="font-semibold">
+                Les énergies renouvelables ne sont pas assez puissantes pour
+                remplacer les combustibles fossiles.
+             </p>{' '}
+             <br></br>
+             FAUX,Les énergies renouvelables sont tout à fait capables de
+             remplacer les combustibles fossiles. En 2020, la capacité mondiale
+             des énergies renouvelables a dépassé 2800 gigawatts, surpassant
+             celle des fossiles. De plus, l'éolien et le solaire connaissent une
+             croissance rapide, avec un potentiel technique bien supérieur à la
+             demande énergétique mondiale actuelle. <br></br>
+             <p className="font-semibold">
+                Le recyclage des déchets n'a pas d'impact significatif sur
+                l'environnement.
+             </p>{' '}
+             <br></br>
+             Faux, Le recyclage a un impact significatif sur l'environnement : il
+             économise jusqu'à 95% de l'énergie pour l'aluminium, environ 40%
+             pour le papier et jusqu'à 70% pour le plastique, réduisant ainsi les
+             émissions de gaz à effet de serre. De plus, le recyclage d'une tonne
+             de papier peut sauver environ 17 arbres, démontrant l'efficacité du
+             recyclage dans la conservation des ressources naturelles et la
+             réduction de la pollution. <br></br>
+             <p className="font-semibold">
+                L'utilisation d'engrais chimiques n'a pas d'effet sur le
+                changement climatique.
+             </p>{' '}
+             <br></br>
+             Faux, L'utilisation d'engrais chimiques contribue au changement
+             climatique : les engrais azotés émettent du protoxyde d'azote, un
+             gaz à effet de serre 300 fois plus puissant que le CO2, et
+             représentent environ 60% des émissions mondiales de N2O. De plus, la
+             production d'engrais consomme 1% à 2% de l'énergie mondiale,
+             augmentant les émissions de gaz à effet de serre. <br></br>
+             <p className="font-semibold">
+                Les avions sont plus écologiques que les trains pour les voyages
+                longue distance.
+             </p>{' '}
+             <br></br>
+             Faux, Pour les voyages longue distance, les trains sont généralement
+             plus écologiques que les avions. Les avions émettent environ 150-200
+             grammes de CO2 par kilomètre et par passager, tandis que les trains
+             émettent moins de 50 grammes, surtout s'ils sont électriques et
+             alimentés par des énergies renouvelables. Les trains sont donc plus
+             efficaces en termes d'émissions de CO2 et d'efficacité énergétique.{' '}
+             <br></br>
+             <p className="font-semibold">
+                La consommation d'eau n'a pas d'impact sur le changement
+                climatique.
+             </p>{' '}
+             <br></br>
+             Faux,La consommation d'eau a un impact sur le changement climatique.
+             Par exemple, la consommation d'eau résidentielle aux États-Unis
+             représente environ 5% des émissions de CO2 du secteur résidentiel.
+             De plus, l'irrigation agricole et la déforestation liées à l'eau
+             contribuent aux émissions de gaz à effet de serre.
+             <p className="font-semibold">
+                {' '}
+                Le tri des déchets est inutile car tout finit dans la même
+                décharge.
+             </p>{' '}
+             <br></br>
+             Faux, Le tri des déchets est essentiel car il augmente le recyclage,
+             réduit les déchets en décharge et économise de l'énergie. Par
+             exemple, environ 25% des déchets municipaux sont recyclés aux
+             États-Unis, et le recyclage de l'aluminium peut économiser jusqu'à
+             95% d'énergie. <br></br>
+             <p className="underline">Idées Véridiques</p> <br></br>
+             <p className="font-semibold">
+                Réduire notre consommation de viande peut avoir un impact
+                significatif sur la réduction des émissions de gaz à effet de
+                serre.{' '}
+             </p>
+             <br></br>
+             Vrai, Réduire la consommation de viande a un impact significatif sur
+             la réduction des émissions de gaz à effet de serre. Environ 14,5%
+             des émissions mondiales de gaz à effet de serre sont dues à
+             l'élevage, ce qui dépasse les émissions de tous les véhicules dans
+             le monde. En conséquence, la réduction de la demande de viande
+             contribue à atténuer ces émissions. <br></br>
+             <p className="font-semibold">
+                La déforestation contribue de manière significative aux émissions
+                de CO2.{' '}
+             </p>
+             <br></br>
+             Vrai, La déforestation contribue significativement aux émissions de
+             CO2, représentant environ 10% des émissions mondiales. En supprimant
+             les arbres, elle libère du carbone stocké dans la biomasse
+             forestière et les sols, contribuant ainsi au changement climatique.{' '}
+             <br></br>
+             <p className="font-semibold">
+                Les petits gestes quotidiens de chacun peuvent cumulativement
+                avoir un grand impact sur l'environnement.
+             </p>
+             <br></br>
+             Vrai, Les petits gestes quotidiens de chacun ont un grand impact sur
+             l'environnement. Par exemple, éteindre les lumières inutiles et
+             économiser de l'énergie peut représenter des centaines de
+             kilowattheures par an pour une seule personne. De même, le
+             recyclage, la réduction des déchets et la préservation de l'eau sont
+             des actions simples qui, lorsqu'adoptées par beaucoup, ont un effet
+             significatif sur l'environnement.<br></br>
+             <p className="font-semibold">
+                L'énergie nucléaire émet peu de CO2 et peut aider à la transition
+                énergétique.
+             </p>
+             <br></br>
+             Vrai, L'énergie nucléaire émet très peu de CO2 pendant son
+             fonctionnement. Environ 12 grammes de CO2 sont émis par
+             kilowattheure d'électricité nucléaire produite, comparé à environ
+             500 à 1000 grammes pour le charbon et le gaz naturel. Cela montre
+             que l'énergie nucléaire est une option à faible émission de carbone.
+             <br></br>
+             <p className="font-semibold">
+                La plantation d’arbres aide à absorber le CO2 de l'atmosphère.
+             </p>
+             <br></br>
+             VRAI, en effet, en plantant des arbres , on crée des puits de
+             carbone naturels qui absorbent une partie de nos rejets de dioxyde
+             de carbone. <br></br>
+             <p className="font-semibold">
+                L'éducation et la sensibilisation au changement climatique sont
+                essentielles pour favoriser l'action.
+             </p>{' '}
+             <br></br>
+             Vrai, L'éducation et la sensibilisation au changement climatique
+             sont essentielles pour favoriser l'action. Par exemple, environ 64%
+             des Américains considèrent le changement climatique comme une
+             préoccupation personnelle importante, montrant l'impact de la
+             sensibilisation sur l'opinion publique et les actions individuelles.{' '}
+             <br></br>
+             <p className="font-semibold">
+                L'utilisation des transports en commun et le covoiturage peuvent
+                réduire considérablement les émissions de gaz à effet de serre.
+             </p>
+             <br></br>
+             Vrai,L'utilisation des transports en commun et le covoiturage
+             réduisent les émissions de gaz à effet de serre de manière
+             significative. Par exemple, un bus peut émettre 20% moins de CO2 par
+             passager-kilomètre que la voiture individuelle, et le covoiturage
+             permet de diviser par plusieurs fois le nombre de voitures sur la
+             route, réduisant ainsi les émissions. <br></br>
+             <p className="font-semibold">
+                Les produits locaux et de saison réduisent l'empreinte carbone
+                liée au transport des aliments.
+             </p>
+             <br></br>
+             Vrai, Les produits locaux et de saison réduisent l'empreinte carbone
+             liée au transport des aliments. Par exemple, le transport de
+             produits locaux nécessite moins d'énergie car les distances sont
+             plus courtes, ce qui peut réduire les émissions de CO2 de manière
+             significative. <br></br>
+             <p className="font-semibold">
+                La biodiversité joue un rôle crucial dans la régulation du climat
+                et le maintien des écosystèmes.
+             </p>{' '}
+             <br></br>
+             Vrai La biodiversité est essentielle pour la régulation du climat et
+             le maintien des écosystèmes. Par exemple, les forêts stockent
+             environ 80% du carbone terrestre, tandis que les récifs coralliens
+             abritent près de 25% de la biodiversité marine. De plus, les
+             services écosystémiques fournis par la biodiversité, tels que la
+             pollinisation des cultures, sont estimés à une valeur économique
+             mondiale d'environ 125 milliards de dollars par an. Ces données
+             soulignent l'importance de préserver la biodiversité pour notre
+             planète.
+          </p>
+       </div>
+    );
+ }
