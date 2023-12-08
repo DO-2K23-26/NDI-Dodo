3a027579e717d1deb6092cf666cee652883cbcf3
Isalyne <isalyne.llinares-rames@etu.umontpellier.fr>
2023-12-08 04:33:31 +0100

    Make sources page

--- src/app/source/page.js
+++ src/app/source/page.js
@@ -1,7 +1,24 @@
 export default function page() {
     return (
         <div className="global">
-            <p>Source Page</p>
+        <div className="p-6  mx-auto bg-white rounded-xl shadow-md ">
+    <h1 className="text-xl font-bold text-blue-600">Sources <br></br></h1>
+        <p className=" leading-8 "> <p className=" font-semibold mt-4" >Changement climatique :</p> 
+        <a href="https://reseauactionclimat.org/urgence-climatique/ ">https://reseauactionclimat.org/</a><br></br>
+        <a href="https://climat.cned.fr/formations/ <br></br>">https://climat.cned.fr/</a><br></br>
+
+        <p className=" font-semibold mt-4" >Solutions face au changement climatique (rapport du GIEC 6) </p>
+        <a href="https://reseauactionclimat.org/6e-rapport-du-giec-quelles-solutions-face-au-changement-climatique/ ">https://reseauactionclimat.org/</a><br></br>
+       
+        <p className=" font-semibold mt-4" >Les fonds nationaux accélèrent la transition écologique</p> 
+        <a href="https://www.ademe.fr/">https://www.ademe.fr/</a> <br></br>
+        
+        <p className=" font-semibold mt-4" >Simulateur d’empreinte carbone :</p> 
+        <a href="https://nosgestesclimat.fr/">https://nosgestesclimat.fr/</a> <br></br>
+
+        ucrgene-climatique/
+        </p>
+        </div>
         </div>
     )
   }
