63edef0fa12b51e8fa1397aceaa3009b3653254b
Isalyne <isalyne.llinares-rames@etu.umontpellier.fr>
2023-12-08 01:31:54 +0100

    setup footer and header

---
+++ src/app/ui/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'; // Assurez-vous de créer ce fichier CSS
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2023 Conçu et développé par PONTHIEU Hugo, PLANCHE Benoit, LABEYRIE Louis & LLINARES Isalyne</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/app/ui/header.js
@@ -0,0 +1,22 @@
+import newt from "../../../public/next.svg"
+import './header.css';
+export default function Header() {
+    return (
+    <>
+         <header className="header">
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item"><a href="#home">Accueil</a></li>
+          <li className="nav-item"><a href="#about">Jeu</a></li>
+          <li className="nav-item"><a href="#services">Solutions</a></li>
+          <li className="nav-item"><a href="#contact">Sources</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+ 
+    
+    </>
+    )
+  }
+  
