7c500031d29667f360699c304f34ae9b682b4c5b
Louis Labeyrie <labeyrielouis@gmail.com>
2023-12-08 02:54:55 +0100

    Add routes

--- src/app/ui/footer.js
+++ src/app/ui/footer.js
@@ -1,11 +1,14 @@
 import './footer.css'; // Assurez-vous de créer ce fichier CSS
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2023 Conçu et développé par PONTHIEU Hugo, PLANCHE Benoit, LABEYRIE Louis & LLINARES Isalyne</p>
-    </footer>
-  );
+	return (
+		<footer className='footer'>
+			<p>
+				© 2023 Conçu et développé par PONTHIEU Hugo, PLANCHE Benoit,
+				LABEYRIE Louis & LLINARES Isalyne
+			</p>
+		</footer>
+	);
 }
 
 export default Footer;
